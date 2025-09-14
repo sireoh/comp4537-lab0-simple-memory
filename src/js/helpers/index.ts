4be4911b13a470a5e64addaa43a4b3f4f9057541
Vincent <eohndk@gmail.com>
2025-09-14 13:54:27 -0700

    added index  class and function

---
+++ src/js/helpers/index.ts
@@ -0,0 +1,22 @@
+import { User } from "../../lang/messages/en/user.js";
+
+export class Index {
+  static mainLabel: HTMLElement | null = document.getElementById("mainLabel");
+  static goLabel: HTMLElement | null = document.getElementById("goLabel");
+
+  static init() {
+    // Initialize the main label
+    if (Index.mainLabel) {
+      Index.mainLabel.innerHTML = User.STARTING_MESSAGE;
+    } else {
+      console.log("Missing mainLabel");
+    }
+
+    // Initialize the go label
+    if (Index.goLabel) {
+      Index.goLabel.innerHTML = User.GO_LABEL;
+    } else {
+      console.log("Missing goLabel");
+    }
+  }
+}
